30c845029134e9acabffbea622e52a726187e2c5
Akihito KIRISAKI <kirisaki@klaraworks.net>
2018-11-19 05:04:49 +0900

    Copy sample code http://agtn.hatenablog.com/entry/2015/07/23/214344

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
 module Main where
 
-import Lib
+import Network.Socket
+import Control.Monad
+import Control.Concurrent
+import Control.Exception
 
 main :: IO ()
-main = someFunc
+main = do
+    soc <- serveSocket 8080
+    listen soc 5
+    acceptLoop soc `finally` close soc
+
+serveSocket :: PortNumber -> IO Socket
+serveSocket port = do
+    soc <- socket AF_INET Stream defaultProtocol
+    addr <- inet_addr "0.0.0.0"
+    bind soc (SockAddrInet port addr)
+    return soc
+
+acceptLoop :: Socket -> IO ()
+acceptLoop soc = forever $ do
+    (conn, addr) <- accept soc
+    forkIO $ echoLoop conn
+
+echoLoop :: Socket -> IO ()
+echoLoop conn = do
+    sequence_ $ repeat $ do
+      (str, _, _) <- recvFrom conn 2
+      send conn str
+    `catch` (\(SomeException e) -> return ())
+    `finally` close conn
