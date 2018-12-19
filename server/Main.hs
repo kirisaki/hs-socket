9f367805151372488dc7db71d10e3bc89d649f46
Ryusuke ENDO <endouryusuke@realglobe.jp>
2018-12-19 20:41:09 +0900

    Parse streaming data

--- server/Main.hs
+++ server/Main.hs
@@ -1,34 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import Network.Socket hiding (send, sendTo, recv, recvFrom)
-import Network.Socket.ByteString.Lazy
+import Prelude hiding (putStrLn)
+import Data.Attoparsec.ByteString
+import Network.Socket 
+import Data.ByteString.Lazy
 import Control.Monad
 import Control.Concurrent
 import Control.Exception.Safe
+import Foreign.Storable
+import System.IO
+import Control.Monad.Managed
+import GHC.IO.Buffer
+import Foreign.Storable
+import Data.Word
+import Data.Char
+import Data.ByteString.Unsafe
 
 main :: IO ()
 main = do
-  soc <- serveSocket
-  sendAll soc "GET http:/v1.39/containers/json"
-  print =<< Network.Socket.ByteString.Lazy.getContents soc
-  
-serveSocket :: IO Socket
-serveSocket = do
-    soc <- socket AF_UNIX Stream defaultProtocol
-    connect soc (SockAddrUnix "/var/run/docker.sock")
-    return soc
+  soc <- serveSocket 8080
+  listen soc 5
+  acceptLoop soc `finally` close soc
+
+serveSocket :: PortNumber -> IO Socket
+serveSocket port = do
+  soc <- socket AF_INET Stream defaultProtocol
+  addr <- inet_addr "0.0.0.0"
+  bind soc (SockAddrInet port addr)
+  return soc
 
 acceptLoop :: Socket -> IO ()
 acceptLoop soc = forever $ do
-    (conn, addr) <- accept soc
-    forkIO $ echoLoop conn
+  (soc', _) <- accept soc
+  buf <- newByteBuffer 4 ReadBuffer
+  forkFinally (echo soc' buf) (\e -> do
+                                print e
+                                )
+  where
+    parser = takeTill (== 0x20)
+    ignitor = parse parser ""
+    echo :: Socket -> Buffer Word8 -> IO ()
+    echo soc buf = withBuffer buf $ \ptr -> loop ptr ignitor
+      where
+        loop ptr result = do
+          len <- recvBuf soc ptr 4
+          str <- unsafePackCStringFinalizer ptr len (return ())
+          let newResult = feed result str
+          print newResult
+          loop ptr newResult
+      
 
-echoLoop :: Socket -> IO ()
-echoLoop conn = do
-    sequence_ $ repeat $ do
-      str <- recv conn 4096
-      send conn str
-    `catch` (\(SomeException e) -> return ())
-    `finally` close conn
+
+
