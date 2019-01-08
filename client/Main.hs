ab0b071e9b4eb584dcaa032090e3fe67873548b8
Ryusuke ENDO <endouryusuke@realglobe.jp>
2019-01-08 12:10:58 +0900

    Chore drinkry instead of pipes

--- client/Main.hs
+++ client/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Exception
+import Control.Monad
+import Data.ByteString
+import Network.Socket hiding (recv)
+import Network.Socket.ByteString (recv, sendAll)
+
 main :: IO ()
-main = putStrLn "client"
+main = withSocketsDo $ do
+    addr <- resolve "localhost" "8080"
+    bracket (open addr) close talk
+  where
+    resolve host port = do
+      let hints = defaultHints { addrSocketType = Stream }
+      addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+      return addr
+    open addr = do
+      sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+      connect sock $ addrAddress addr
+      return sock
+    talk sock = do
+      replicateM_ 3 $ sendAll sock "1234567890"
--- server/Main.hs
+++ server/Main.hs
@@ -1,33 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Strict #-}
 module Main where
 
-import Prelude
-import Data.Attoparsec.ByteString
-import Network.Socket 
-import Data.ByteString hiding (putStrLn)
+import Data.Drinkery
+import Data.ByteString
+import Data.ByteString.Internal
 import Control.Monad
+import Control.Monad.IO.Class
 import Control.Concurrent
 import Control.Exception.Safe
-import Foreign.Storable
-import Control.Monad.Managed
-import Control.Monad.State.Strict
+import Data.Attoparsec.ByteString as A
+import Network.Socket as S 
+import Foreign.Ptr
 import GHC.IO.Buffer
 import Foreign.Storable
 import Data.Word
-import Data.Char
-import Data.ByteString.Unsafe
-import Pipes as P
-import qualified Pipes.Prelude as P
-import Pipes.Attoparsec hiding (parse)
-import Pipes.Parse
-import Foreign.Ptr
-import Control.Monad.IO.Class
 
 main :: IO ()
 main =  do
   soc <- serveSocket 8080
   listen soc 5
-  acceptLoop soc `finally` close soc
+  acceptLoop soc `finally` S.close soc
 
 serveSocket :: PortNumber -> IO Socket
 serveSocket port = do
@@ -38,49 +31,42 @@ serveSocket port = do
 
 acceptLoop :: Socket -> IO ()
 acceptLoop soc = forever $ do
-  (soc', _) <- accept soc
+  (soc', _) <- S.accept soc
   buf <- newByteBuffer 4 ReadBuffer
-  forkFinally (echo soc' buf) (\e -> do
+  forkFinally (server soc' buf) (\e -> do
                                 print e
                                 )
-  where
-    parser = Data.Attoparsec.ByteString.take 10
-    parser' :: Pipes.Parse.Parser ByteString IO (Maybe (Either ParsingError (Int, ByteString)))
-    parser' = parseL parser
-    echo :: Socket -> Buffer Word8 -> IO ()
-    echo soc buf = withBuffer buf $ \ptr -> do
-      forever $ do
-         runStateT (runEffect $ loop ptr >-> consumer) (parse parser "")
-      where
-        consumer = do
-          val <- P.await
-          liftIO $ print val
-        feedRepeatly :: Result ByteString -> ByteString -> Producer ByteString (StateT (Result ByteString) IO) ()
-        feedRepeatly result str =
-          case feed result str of
-            Done left val -> do
-              if left == ""
-                then return ()
-                else feedRepeatly (parse parser "") left
-              lift $ put (parse parser left)
-              P.yield val
-            Partial r ->
-              lift $ put (Partial r)
-            Fail _ _ _ -> do
-              liftIO $ print "nyaan"
-              lift $ put (parse parser "")
-        loop :: Ptr Word8 -> Producer ByteString (StateT (Result ByteString) IO) ()
-        loop ptr = do
-          len <- liftIO $ recvBuf soc ptr 4
-          str <- liftIO $ unsafePackCStringFinalizer ptr len (return ())
-          result <- lift $ get
-          feedRepeatly result str
-          loop ptr
-        loop' ptr = do
-          len <- lift $ recvBuf soc ptr 4
-          str <- lift $ unsafePackCStringFinalizer ptr len (return ())
-          P.yield str
-          loop' ptr
-      
 
+server :: Socket -> Buffer Word8 -> IO ()
+server soc buf = withBuffer buf $ \ptr -> socketTap soc ptr +& consumer
+
+consumer :: Sink (Tap () ByteString) IO ()
+consumer =
+  let
+    parser = A.take 10
+    parseRepeatedly = do
+      i <- consume
+      case parse parser i of
+        Done "" r ->
+          liftIO $ print r
+        Done i' r -> do
+          leftover i'
+          liftIO $ print r
+        Partial _ -> do
+          i' <- consume
+          leftover (i <> i')
+        _ ->
+          liftIO $ print "nyaan"      
+  in do
+    parseRepeatedly
+    consumer
+
+type SocketTap = Tap () ByteString IO
+
+socketTap :: Socket -> Ptr Word8 -> SocketTap
+socketTap soc ptr = repeatTapM' io
+  where
+    io = do
+      len <- recvBuf soc ptr 4
+      create len $ \buf -> memcpy buf ptr len
 
